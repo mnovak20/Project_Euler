3c527468a4768d4e102891a8781f8091819c0f9a
Mike <mnovak419@gmail.com>
2015-01-10 22:25:03 -0500

    PE7

--- PE7.js
+++ PE7.js
@@ -1,4 +1,31 @@
 /*By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, 
 we can see that the 6th prime is 13.
 
-What is the 10 001st prime number?*/
+What is the 10001st prime number?*/
+
+
+
+var isPrime = function(num){
+    for(var i = 2; i < num; i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+        return true;
+};
+
+
+
+function prime10001(){
+    var result = [];
+    var i = 1;
+    while(result.length < 10002){
+        if(isPrime(i)){
+            result.push(i);
+        }
+        i++;
+    }
+    console.log(result[10001]);
+}
+
+prime1001();
